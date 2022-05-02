01de101e224393a053fd2baee4c4108f1f0e6fc1
Sunny Lac <23119307@student.uwa.edu.au>
2022-05-02 21:45:37 +0800

    Made it work with pc keyboard

--- keyboard.js
+++ keyboard.js
@@ -1,11 +1,34 @@
 function press(letter){
     const text = document.getElementById("textBox");
-    if (letter.innerHTML != "DELETE" && letter.innerHTML != "ENTER"){
-        text.innerHTML += letter.innerHTML;
+    if (letter != "DELETE" && letter != "ENTER"){
+        text.innerHTML += letter;
     }
     else{
-        if (letter.innerHTML == "DELETE"){
+        if (letter == "DELETE"){
             text.innerHTML = text.innerHTML.slice(0,-1);
+        }
+        if(letter == "ENTER"){
+            //method
         }
     }
 }
+
+
+document.addEventListener('keydown', function(event){
+    let letter = event.key;
+    if (letter == "Backspace"){
+        press("DELETE");
+        return;
+    }
+    if (letter == "Enter"){
+        //method
+        return;
+    }
+    if ((letter >= 'a' && letter <= 'z') || letter >= 'A' && letter <= 'Z'){
+    if (letter >= 'a' && letter <= 'z'){
+        letter = letter.toUpperCase();
+    }
+    press(letter);
+}
+
+})

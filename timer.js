0bacc263a99d60a68e5c2fa3d8838a2ca812e5e9
Jeremy Robertson <23279784@student.uwa.edu.au>
2022-05-09 15:31:22 +0800

    Wrote JS code for timer functions

---
+++ timer.js
@@ -0,0 +1,16 @@
+document.getElementById("begin").addEventListener("click", function(){
+    var timeleft = 60;
+
+    var downloadTimer = setInterval(function function1(){
+    document.getElementById("countdown").innerHTML = timeleft + 
+    "&nbsp"+"seconds remaining";
+
+    timeleft -= 1;
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "Time is up!"
+    }
+    }, 1000);
+
+    console.log(countdown);
+});

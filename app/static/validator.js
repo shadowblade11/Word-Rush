204aabab6fa6838aed2299e9edc200ad7f48c66d
Sunny Lac <23119307@student.uwa.edu.au>
2022-05-10 11:51:22 +0800

    redid the functions, still does not work

--- app/static/validator.js
+++ app/static/validator.js
@@ -1,24 +1,59 @@
+var globalState;
 function getWord(word){
     console.log(word);
+    gWord = word;
     // console.log(validateWord(word));
+    // console.log(doStuff());
     if(validateWord(word)){
-        console.log("he");
+        console.log("hello");
         if(checkIfWordInArray(word)){
             addWord(word);
         }
     }
 }
-
 function validateWord(word){
-    const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
-    const http = new XMLHttpRequest();
-    http.open("GET", url);
-    http.send();
-    var state = false;
-    http.onload = function(){state = (http.status != 404)};
-    console.log(state);
+    // console.log(state);
+    // return state;
+    // doStuff(word);
+    var Finalresult = false;
+    console.log(doStuff(word));
+    console.log("Global state is "+globalState);
+    return Finalresult;
 }
 
+function makeAjaxCall(word, callback) {
+    const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
+    $.ajax(url)
+        .done( 
+            function() {
+                // Do stuff
+                console.log("works");
+                callback(true);
+            }
+        )
+        .fail(
+            function() {
+                console.log("fails");
+                callback(false);
+            }
+        );
+}
+function doStuff(word) {
+var a = makeAjaxCall( word, function(result) {
+    // return result;
+    if (result == true){
+        console.log("it worked");
+        globalState = true;
+        return true;
+    }
+    else{
+        console.log("it didn't work")
+        globalState = false;
+        return false;
+    }
+    });
+    return a;
+}
 
 function checkIfWordInArray(word){
     return true;

d5547f1bbfa338f7c406312d1c9dd651a73cf1dd
Jeremy Robertson <23279784@student.uwa.edu.au>
2022-05-17 21:09:16 +0800

    Added music id here, so music will now start when the popup is closed

--- app/static/Popup.js
+++ app/static/Popup.js
@@ -16,6 +16,7 @@ $(document).mouseup(function () {
     var popup = $(".popup");
     popup.hide(500);
     document.querySelector(".container-popup").style.display = "none";
+    document.getElementById("audio").play();
 });
 
 //Needed for button

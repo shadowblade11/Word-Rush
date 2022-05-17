9f0f6b6695ae1670499e9be21cf6a9f45016cca6
Sunny Lac <23119307@student.uwa.edu.au>
2022-05-17 17:17:45 +0800

    started login page (NOT FINISHED)

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for, flash, redirect
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    return render_template("login.html")

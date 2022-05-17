9dd170016a70546619b53832702a62860daa4e3c
Jeremy Robertson <23279784@student.uwa.edu.au>
2022-05-17 14:52:31 +0800

    Made some changes to popup to work on outside click

--- app/static/Popup.js
+++ app/static/Popup.js
@@ -3,15 +3,27 @@ window.addEventListener("load", function(){
         function open(event){
             document.querySelector(".popup").style.display = "block";
         },
-        500
+        250
     )
 });
 
+//Click outside to close using jQuery
+$(document).ready(function () {
+    $('.popup').hide()
+});
 
+$(document).mouseup(function () {
+    var popup = $(".popup");
+    popup.hide(500);
+    document.querySelector(".container-popup").style.display = "none";
+});
+
+//Needed for button
 document.querySelector("#close").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "none";
     document.querySelector(".container-popup").style.display = "none";
 });
+
 
 
 

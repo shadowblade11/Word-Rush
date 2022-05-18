d07d35e25f09afd513bd567f5632044bc15ecc39
Sunny Lac <23119307@student.uwa.edu.au>
2022-05-18 21:14:02 +0800

    app.db is always gonna change due to me adding more users, tried some tests in dummyTest

--- dummyTest.py
+++ dummyTest.py
@@ -1,7 +1,10 @@
+from select import select
 from app.models import User, History
 from app import db
 import datetime
 from datetime import date
+from datetime import datetime
+from sqlalchemy import desc
 
 
 # u = User(username="nic", password_hash="123")
@@ -14,13 +17,20 @@ from datetime import date
 
 
 # 1 USER CAN HAVE MULTIPLE HISTORY (MATCHES) 
+# date_time_str = '18/09/2021 00:00:00'
 
-# h = History(user_id=1,date=date.today(),points=100,wordAccuracy=50,totalWords=1000,letters="EF")
+# date_time_obj = datetime.strptime(date_time_str, '%d/%m/%Y %H:%M:%S')
+# h = History(user_id=1,date=date_time_obj,points=100,wordAccuracy=50,totalWords=1000,letters="ZZ")
 # db.session.add(h)
 # db.session.commit()
 
 
-user1 = User.query.get(1)
-print(user1.getHistory())
-
+user1 = User.query.get(3)
+print(user1.histories)
+curdate = str(datetime.today().strftime('%d/%m/%Y')) +" 00:00:00"
+formated_date = datetime.strptime(curdate, '%d/%m/%Y %H:%M:%S')
+print(formated_date)
+for i in user1.histories:
+    print(i.date)
+    print(str(i.date==formated_date))
 # print(date.today())

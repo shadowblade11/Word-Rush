09b16d7c1b9f7eaa667c8e2ff3154109e4aedffd
Sunny Lac <23119307@student.uwa.edu.au>
2022-05-19 23:12:54 +0800

    reduced music volume (was way too loud before)

--- app/static/Popup.js
+++ app/static/Popup.js
@@ -17,6 +17,7 @@ $(document).mouseup(function () {
     popup.hide(500);
     document.querySelector(".container-popup").style.display = "none";
     document.getElementById("audio").play();
+    document.getElementById("audio").volume = 0.03;
 });
 
 

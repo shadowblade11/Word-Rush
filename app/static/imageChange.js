d2f06f42d39a21e8928df4165eb6e982699737cf
Jeremy Robertson <23279784@student.uwa.edu.au>
2022-05-20 03:18:17 +0800

    Made it so music will now load on website load

--- app/static/imageChange.js
+++ app/static/imageChange.js
@@ -3,6 +3,8 @@ document.getElementById("myImg").addEventListener("click", imageChange());
 function imageChange()
 {
     if( document.getElementById("myImg").src == "https://w7.pngwing.com/pngs/131/14/png-transparent-audio-mute-sound-speaker-volume-basic-web-elements-icon.png" ){
+        document.getElementById("audio").play();
+        document.getElementById("audio").volume = 0.03;
         var img = document.getElementById("myImg").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Speaker_Icon.svg/750px-Speaker_Icon.svg.png"
         img.style.height = '100';
         img.style.width = '100';

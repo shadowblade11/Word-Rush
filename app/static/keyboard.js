6e1d936e9e061359307542d1990ffeca75731c3c
Sunny Lac <23119307@student.uwa.edu.au>
2022-05-20 13:16:05 +0800

    made the keyboard onload (remove <body onload> for later on)

--- app/static/keyboard.js
+++ app/static/keyboard.js
@@ -1,4 +1,7 @@
 const keyboardKeys = [['Q','W','E','R','T','Y','U','I','O','P'],['A','S','D','F','G','H','J','K','L'],['DELETE','Z','X','C','V','B','N','M','ENTER']];
+window.addEventListener("load", function(){
+    initKeyboard();
+});
 function initKeyboard(){
     let mainDiv = document.getElementById("main");
     for (let row = 0; row < keyboardKeys.length;row++){
